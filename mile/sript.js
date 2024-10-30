6973a39722af96729c19ce177e39181ecba6abff
William Estrada <estrada5686@live.com>
2024-10-29 22:31:55 -0500

    Add files via upload

---
+++ mile/sript.js
@@ -0,0 +1,8 @@
+function mile(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Hola Mile de mi amor',
+        text: 'Este es un mensaje especial para ti.',
+        confirmButtonText: 'Aceptar'
+    });
+}
